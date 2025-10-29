5ea6ced22187e647a71e084fbeb9df369c2f3148
Sumit Gupta <cricsumit24@gmail.com>
2025-10-29 22:39:17 +0530

    update cors origin

--- server.js
+++ server.js
@@ -6,9 +6,9 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 const userRoutes = require('./routes/userRoutes');
 const eventRoutes = require('./routes/eventRoutes');
-const categoryRoutes = require('./routes/categoryRoutes'); // Import category routes
-const adminRoutes = require('./routes/adminRoutes'); // Import admin routes
-const systemSettingRoutes = require('./routes/systemSettingRoutes'); // Import system setting routes
+const categoryRoutes = require('./routes/categoryRoutes');
+const adminRoutes = require('./routes/adminRoutes');
+const systemSettingRoutes = require('./routes/systemSettingRoutes');
 const { notFound, errorHandler } = require('./middleware/errorMiddleware');
 
 const app = express();
@@ -29,22 +29,20 @@ app.use(cors({
   origin: ['http://localhost:5173','https://eventxpert-frontend.vercel.app'],
   credentials: true,
 }));
-app.use(express.json({ limit: '50mb' })); // Increased limit for JSON body
-app.use(express.urlencoded({ limit: '50mb', extended: true })); // Added for URL-encoded bodies and increased limit
+app.use(express.json({ limit: '50mb' })); 
+app.use(express.urlencoded({ limit: '50mb', extended: true })); 
 
 // Routes
 app.use('/api/users', userRoutes);
 app.use('/api/events', eventRoutes);
-app.use('/api/categories', categoryRoutes); // Use category routes
-app.use('/api/admin', adminRoutes); // Use admin routes
-app.use('/api/settings', systemSettingRoutes); // Use system setting routes
-
+app.use('/api/categories', categoryRoutes);
+app.use('/api/admin', adminRoutes); 
+app.use('/api/settings', systemSettingRoutes); 
 app.get('/', (req, res) => {
   res.send('API is running...');
 });
 
 app.use(notFound);
-// Error Handling Middleware
 app.use(errorHandler);
 
 const PORT = process.env.PORT || 4000;
